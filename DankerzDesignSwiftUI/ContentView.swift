cf4a5680c0b15be11c8583d037ebef044fa04cfe
panterasls <panterasls123@yahoo.com>
2020-05-20 18:01:56 +0300

    Initial Commit

---
+++ DankerzDesignSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DankerzDesignSwiftUI
+//
+//  Created by Надія Возна on 20.05.2020.
+//  Copyright © 2020 Надія Возна. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
